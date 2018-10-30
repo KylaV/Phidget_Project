245326756609ab5fcfddb766e4dee74230fde224
kylav1 <kylav1@learn.cssd.ab.ca>
2018-10-30 14:41:30 -0600

    Initial Commit

---
+++ Phidget_Project/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Phidget_Project
+//
+//  Created by Kyla Vidallo on 2018-10-30.
+//  Copyright © 2018 Kyla Vidallo. All rights reserved.
+//
+
+import UIKit
+import Phidget22Swift
+
+class ViewController: UIViewController {
+    
+    let ledArray = [DigitalOutput(), DigitalOutput()]
+    let buttonArray = [DigitalInput(), DigitalInput()]
+    
+//    func button_attach_handler(sender: Phidget) {
+//        do{
+//
+//            let hubPort = try sender.getHubPort()
+//
+//            if (hubPort == 0){
+//                print("Button 0 Attached")
+//            }
+//            else {
+//                print("Button 1 Attached")
+//            }
+//
+//        } catch let err as PhidgetError {
+//            print("Phidget Error" + err.description)
+//        } catch {
+//            //catch other errors here
+//        }
+//    }
+    
+    func led_attach_handler(sender: Phidget) {
+        do{
+            
+            let hubPort = try sender.getHubPort()
+            
+            
+            if (hubPort == 2) {
+                print("LED 2 Attached")
+            }
+            else {
+                print("LED 3 Attached")
+            }
+
+        } catch let err as PhidgetError {
+            print("Phidget Error" + err.description)
+        } catch {
+            //catch other errors here
+        }
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do{
+            
+            try Net.enableServerDiscovery( serverType: .deviceRemote)
+            
+//            for i in 0..<buttonArray.count{
+//                try buttonArray[i].setDeviceSerialNumber(528057)
+//                try buttonArray[i].setHubPort(i)
+//                try buttonArray[i].setIsHubPortDevice(true)
+//                let _ = buttonArray[i].attach.addHandler(button_attach_handler)
+//                try buttonArray[i].open()
+//            }
+//
+            for i in 0..<ledArray.count{
+                try ledArray[i].setDeviceSerialNumber(528057)
+                try ledArray[i].setHubPort(i)
+                try ledArray[i].setIsHubPortDevice(true)
+                let _ = ledArray[i].attach.addHandler(led_attach_handler)
+                try ledArray[i].open()
+            }
+            
+        } catch let err as PhidgetError {
+            print("Phidget Error" + err.description)
+        } catch {
+            //catch other errors here
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Phidget_Project' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Phidget_Project
+  pod 'Phidget22Swift'
+
+end
