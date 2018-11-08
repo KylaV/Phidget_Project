b25253ff658c04ed442276d92fa39c00f66e55f3
kylav1 <kylav1@learn.cssd.ab.ca>
2018-11-08 14:34:49 -0700

    Added Questions

--- Phidget_Project/ViewController.swift
+++ Phidget_Project/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     var redButtonPressed : String = ""
     var rCount : Int = 0
     var gCount : Int = 0
+    var buttonAlreadyPressed : Bool = true
     
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var redButtonCount: UILabel!
     @IBOutlet weak var greenButtonCount: UILabel!
     
+    //Checks if Buttons and LEDs attached
     func attach_handler(sender: Phidget) {
         do{
 
@@ -46,18 +48,23 @@ class ViewController: UIViewController {
         }
     }
     
+    // When red button pressed
     func state_change0 (sender: DigitalInput, state: Bool) {
         do{
             if (state == true){
-                print("Button 0 Pressed")
-                try ledArray[0].setState(true)
+                if (buttonAlreadyPressed == true) {
+                    print("Button 0 Pressed")
+                    print("player one pressed")
+                    buttonAlreadyPressed = false
+                    try ledArray[0].setState(true)
+                    try ledArray[1].setState(false)
+                }
                 redButton()
                 redCount()
                 rCount = rCount + 1
             }
             else {
                 print("Button 0 Not Pressed")
-                try ledArray[1].setState(false)
             }
         } catch let err as PhidgetError {
             print("Phidget Error" + err.description)
@@ -66,19 +73,23 @@ class ViewController: UIViewController {
         }
     }
     
+    //When green button pressed
     func state_change1 (sender: DigitalInput, state: Bool) {
         do{
 
             if (state == true){
+                if (buttonAlreadyPressed == true) {
                 print("Button 1 Pressed")
+                buttonAlreadyPressed = false
                 try ledArray[1].setState(true)
+                try ledArray[0].setState(false)
+                }
                 greenButton()
                 greenCount()
                 gCount = gCount + 1
             }
             else {
                 print("Button 1 Not Pressed")
-                try ledArray[0].setState(false)
             }
         } catch let err as PhidgetError {
             print("Phidget Error" + err.description)
@@ -87,15 +98,20 @@ class ViewController: UIViewController {
         }
     }
     
+//    func nextQuestion() {
+//        
+//    }
+    
+    //Changes the labels on mainviewcontroller
     func redButton() {
         DispatchQueue.main.async {
             self.labelOne.text = "Red Button Pressed"
         }
     }
-
-    func redCount() {
+    
+    func greenButton() {
         DispatchQueue.main.async {
-            self.redButtonCount.text = "Red Button Pressed \(self.rCount)"
+            self.labelOne.text = "Green Button Pressed"
         }
     }
 
@@ -104,10 +120,10 @@ class ViewController: UIViewController {
             self.greenButtonCount.text = "Green Button Pressed \(self.gCount)"
         }
     }
-
-    func greenButton() {
+    
+    func redCount() {
         DispatchQueue.main.async {
-            self.labelOne.text = "Green Button Pressed"
+            self.redButtonCount.text = "Red Button Pressed \(self.rCount)"
         }
     }
 
@@ -116,7 +132,7 @@ class ViewController: UIViewController {
         do{
             
             try Net.enableServerDiscovery( serverType: .deviceRemote)
-            
+            //checks Buttons
             for i in 0..<buttonArray.count{
                 try buttonArray[i].setDeviceSerialNumber(528057)
                 try buttonArray[i].setHubPort(i)
@@ -124,7 +140,7 @@ class ViewController: UIViewController {
                 let _ = buttonArray[i].attach.addHandler(attach_handler)
                 try buttonArray[i].open()
             }
-
+            //checks LEDs
             for i in 0..<ledArray.count{
                 try ledArray[i].setDeviceSerialNumber(528057)
                 try ledArray[i].setHubPort(i + 2)
@@ -132,7 +148,7 @@ class ViewController: UIViewController {
                 let _ = ledArray[i].attach.addHandler(attach_handler)
                 try ledArray[i].open()
             }
-            
+            //state change of buttons
             let _ = buttonArray[0].stateChange.addHandler(state_change0)
             let _ = buttonArray[1].stateChange.addHandler(state_change1)
             
@@ -142,10 +158,6 @@ class ViewController: UIViewController {
             //catch other errors here
         }
         
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
